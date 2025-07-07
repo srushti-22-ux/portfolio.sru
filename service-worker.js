8db252d53cee75f82dfff0aef060dc0b241cc592
srushti-22-ux <soudattisrushtisoudatti@gmail.com>
2025-07-07 15:38:13 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -31,3 +31,46 @@ self.addEventListener('fetch', evt => {
     caches.match(evt.request).then(res => res || fetch(evt.request))
   );
 });
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
